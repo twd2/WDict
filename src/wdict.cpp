628c78db0a5e5a4ef839a0a88d5746f49af18737
Wende Tan <twd2@163.com>
2016-04-30 10:53:45 +0800

    words level

--- src/wdict.cpp
+++ src/wdict.cpp
@@ -8,13 +8,13 @@ int main()
     
     try
     {
-        auto cons = make_shared<Console>();
+        /*auto cons = make_shared<Console>();
         cons->Goto(make_shared<WithTitleConsole>("WDict", make_shared<MainConsole>(*cons)));
         UI::Init(cons);
         UI::Start();
-        return 0;
+        return 0;*/
         
-        TextDB db("dict.txt");
+        TextDB db("dictcn_level_1_.txt");
         
         string s;
         
